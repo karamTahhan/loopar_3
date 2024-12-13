53a95430b0d071d5f3075332e0698dc6b0e25930
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-13 08:52:41 +0100

    Add project files.

---
+++ loopar_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int totalSum = 0;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Ange heltal {i}: ");
+            int number = int.Parse(Console.ReadLine());
+            totalSum += number;
+        }
+
+        Console.WriteLine($"Summan av de inmatade talen är: {totalSum}");
+    }
+}
